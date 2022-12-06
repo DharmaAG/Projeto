83c1dc0d78dd9d8883981bd90a5c92018729d74e
aluno <aluno@172.16.3.18>
2022-12-06 16:24:09 -0300

    Uc feita.

--- ProjetoJava/src/com/ifsc/tds/Uc.java
+++ ProjetoJava/src/com/ifsc/tds/Uc.java
@@ -2,4 +2,36 @@ package com.ifsc.tds;
 
 public class Uc {
 
+	private int ucld;
+	private int leituraAnterior;
+	private int leituraAtual;
+	
+	public void Uc() {}
+
+	public int getUcld() {
+		return ucld;
+	}
+
+	public void setUcld(int ucld) {
+		this.ucld = ucld;
+	}
+
+	public int getLeituraAnterior() {
+		return leituraAnterior;
+	}
+
+	public void setLeituraAnterior(int leituraAnterior) {
+		this.leituraAnterior = leituraAnterior;
+	}
+
+	public int getLeituraAtual() {
+		return leituraAtual;
+	}
+
+	public void setLeituraAtual(int leituraAtual) {
+		this.leituraAtual = leituraAtual;
+	}
+
+	
+	
 }
